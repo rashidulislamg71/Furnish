de1511633388ff18f4856d9bf605d9bca2f27ae2
rashidulislamg71 <jrashidulislam0103@gmail.com>
2025-02-04 07:52:07 +0600

    our products hover efect

--- src/Componente/OurProducts/OurProducts.jsx
+++ src/Componente/OurProducts/OurProducts.jsx
@@ -6,6 +6,11 @@ import chair from "../../assets/Images/Products_img/Chair.jpeg";
 import sofa_set from "../../assets/Images/Products_img/Luxury_sofa_set.png";
 import reception from "../../assets/Images/Products_img/Reception_1.jpg";
 
+
+import { FaOpencart } from "react-icons/fa6";
+import { IoShareSocial } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
+
 const OurProducts = () => {
   return (
     <div className={`${styles.our_products_container}`}>
@@ -22,50 +27,95 @@ const OurProducts = () => {
               <h4>Table</h4>
               <small>Stylish Smart Products.</small>
               <div className={ styles.price}>
-                <p>13,000.00</p>
-                BDT
+                <p>13,000.00 BDT</p>
+                
+              </div>
+            </div>
+
+            {/* product hover content  */}
+            <div className={`${styles.flex} ${styles.product_hover_container}`}>
+              <div className={`${styles.flex} ${styles.cart}`}>
+                <button> <p><FaOpencart /></p> Add to cart</button>
+              </div>
+              <div className={`${styles.flex} ${styles.options}`}>
+                <button className={styles.share}> <p><IoShareSocial /></p> Share</button>
+                <button className={styles.add_favourite}><p><FaRegHeart /></p> Favourite</button>
               </div>
             </div>
           </div>
           <div className={`${styles.single_product}`}>
             <div className={styles.product_img}>
-              <img src={sofa_set} alt="Tables" />
+              <img src={sofa_set} alt="Sofa Set" />
             </div>
             <div className={`${styles.product_detils}`}>
               <h4>Sofa Set</h4>
               <small>Stylish Smart Products.</small>
               <div className={ styles.price}>
-                <p>13,000.00</p>
-                BDT
+                <p>13,000.00 BDT</p>
+                
+              </div>
+            </div>
+
+            {/* product hover content  */}
+            <div className={`${styles.flex} ${styles.product_hover_container}`}>
+              <div className={`${styles.flex} ${styles.cart}`}>
+                <button> <p><FaOpencart /></p> Add to cart</button>
+              </div>
+              <div className={`${styles.flex} ${styles.options}`}>
+                <button className={styles.share}> <p><IoShareSocial /></p> Share</button>
+                <button className={styles.add_favourite}><p><FaRegHeart /></p> Favourite</button>
               </div>
             </div>
           </div>
           <div className={`${styles.single_product}`}>
             <div className={styles.product_img}>
-              <img src={chair} alt="Tables" />
+              <img src={chair} alt="Chair" />
             </div>
             <div className={`${styles.product_detils}`}>
               <h4>Chair</h4>
               <small>Stylish Smart Products.</small>
               <div className={ styles.price}>
-                <p>13,000.00</p>
-                BDT
+                <p>13,000.00 BDT</p>
+                
+              </div>
+            </div>
+
+            {/* product hover content  */}
+            <div className={`${styles.flex} ${styles.product_hover_container}`}>
+              <div className={`${styles.flex} ${styles.cart}`}>
+                <button> <p><FaOpencart /></p> Add to cart</button>
+              </div>
+              <div className={`${styles.flex} ${styles.options}`}>
+                <button className={styles.share}> <p><IoShareSocial /></p> Share</button>
+                <button className={styles.add_favourite}><p><FaRegHeart /></p> Favourite</button>
               </div>
             </div>
           </div>
           <div className={`${styles.single_product}`}>
             <div className={styles.product_img}>
-              <img src={reception} alt="Tables" />
+              <img src={reception} alt="Reception" />
             </div>
             <div className={`${styles.product_detils}`}>
               <h4>Reception</h4>
               <small>Stylish Smart Products.</small>
               <div className={ styles.price}>
-                <p>13,000.00</p>
-                BDT
+                <p>13,000.00 BDT</p>
+                
+              </div>
+            </div>
+
+            {/* product hover content  */}
+            <div className={`${styles.flex} ${styles.product_hover_container}`}>
+              <div className={`${styles.flex} ${styles.cart}`}>
+                <button> <p><FaOpencart /></p> Add to cart</button>
+              </div>
+              <div className={`${styles.flex} ${styles.options}`}>
+                <button className={styles.share}> <p><IoShareSocial /></p> Share</button>
+                <button className={styles.add_favourite}><p><FaRegHeart /></p> Favourite</button>
               </div>
             </div>
           </div>
+ 
         </div>
       </div>
     </div>
